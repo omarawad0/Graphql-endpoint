5373b97c1fc4f080ad18306b38552d87b47cf064
omarawad0 <ogmal01@gmail.com>
2021-10-31 04:37:27 +0200

    fixed start

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from "apollo-server";
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
-
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+const PORT = process.env.port || 4000;
 const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
+  typeDefs: typeDefs,
+  resolvers: resolvers,
 });
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: PORT }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
